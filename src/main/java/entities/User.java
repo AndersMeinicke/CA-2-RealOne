c5982ff27e61dc2245e08fcedd39d0958c039620
Oliverden3 <oliversjensen@hotmail.com>
2022-11-07 13:10:19 +0100

    Auto generating id in user

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,9 +1,6 @@
 package entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,6 +8,7 @@ import javax.validation.constraints.Size;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
     private Integer id;
 
