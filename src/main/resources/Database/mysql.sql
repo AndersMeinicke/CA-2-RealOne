841a1b67bbfc4c70d45f6fcad96645c7d282d889
Oliverden3 <oliversjensen@hotmail.com>
2022-11-08 10:54:09 +0100

    Now SQL works

--- src/main/resources/Database/mysql.sql
+++ src/main/resources/Database/mysql.sql
@@ -4,15 +4,34 @@
 -- ------------------------------------------------------
 -- Server version	8.0.30
 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema ca2_db
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS ca2_db ;
+
+-- -----------------------------------------------------
+-- Schema ca2_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS ca2_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE ca2_db ;
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE=''+00:00'' */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=''NO_AUTO_VALUE_ON_ZERO'' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
@@ -22,10 +41,9 @@
 DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `roles`
-(
-    `role_name` varchar(20) NOT NULL DEFAULT '' user '',
-    PRIMARY KEY (`role_name`)
+CREATE TABLE `roles` (
+                         `role_name` varchar(20) NOT NULL DEFAULT 'user',
+                         PRIMARY KEY (`role_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -36,12 +54,11 @@ CREATE TABLE `roles`
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users`
-(
-    `user_name` varchar(25) NOT NULL,
-    `user_pass` varchar(255) DEFAULT NULL,
-    `user_id`   int         NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`user_id`)
+CREATE TABLE `users` (
+                         `user_name` varchar(25) NOT NULL,
+                         `user_pass` varchar(255) DEFAULT NULL,
+                         `user_id` int NOT NULL AUTO_INCREMENT,
+                         PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -52,16 +69,15 @@ CREATE TABLE `users`
 DROP TABLE IF EXISTS `users_has_roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users_has_roles`
-(
-    `users_user_id`   int         NOT NULL,
-    `roles_role_name` varchar(20) NOT NULL,
-    `JoinedTableID`   int         NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`JoinedTableID`),
-    KEY               `fk_users_has_roles_roles1_idx` (`roles_role_name`),
-    KEY               `fk_users_has_roles_users_idx` (`users_user_id`),
-    CONSTRAINT `fk_users_has_roles_roles1` FOREIGN KEY (`roles_role_name`) REFERENCES `roles` (`role_name`),
-    CONSTRAINT `fk_users_has_roles_users` FOREIGN KEY (`users_user_id`) REFERENCES `users` (`user_id`)
+CREATE TABLE `users_has_roles` (
+                                   `users_user_id` int NOT NULL,
+                                   `roles_role_name` varchar(20) NOT NULL,
+                                   `JoinedTableID` int NOT NULL AUTO_INCREMENT,
+                                   PRIMARY KEY (`JoinedTableID`),
+                                   KEY `fk_users_has_roles_roles1_idx` (`roles_role_name`),
+                                   KEY `fk_users_has_roles_users_idx` (`users_user_id`),
+                                   CONSTRAINT `fk_users_has_roles_roles1` FOREIGN KEY (`roles_role_name`) REFERENCES `roles` (`role_name`),
+                                   CONSTRAINT `fk_users_has_roles_users` FOREIGN KEY (`users_user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
